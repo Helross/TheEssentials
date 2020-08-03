e7e93a6ce3533f9026bdd21bf1cf427a5ba71fe6
Helross <heloross@gmail.com>
2020-08-03 15:18:14 +0200

    Basic, event-based input manager system with simple subclass example and tester class.

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+
+namespace CodeOverload
+{
+    public class InputManager : MonoBehaviour
+    {
+        private static InputManager _instance;
+
+        [SerializeField] private KeyCode _action1Key;
+        [SerializeField] private KeyCode _action1KeyAlternate;
+        [SerializeField] private KeyCode _action2Key;
+        [SerializeField] private KeyCode _action2KeyAlternate;
+
+        private event Action Action1Event;
+        private event Action Action2Event;
+
+        [SerializeField] private PlayerInput _player1Controls;
+        [SerializeField] private PlayerInput _player2Controls;
+
+        private void Awake() => _instance = this;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_action1Key) || Input.GetKeyDown(_action1KeyAlternate)) Action1Event?.Invoke();
+            if (Input.GetKeyDown(_action2Key) || Input.GetKeyDown(_action2KeyAlternate)) Action2Event?.Invoke();
+
+            if (Input.GetKeyDown(_player1Controls._moveForward)) _player1Controls.InvokeForward();
+        }
+
+        public static void SubscribeToAction1(Action callback) => _instance.Action1Event += callback;
+        public static void UnsubscribeFromAction1(Action callback) => _instance.Action1Event -= callback;
+        public static void SubscribeToAction2(Action callback) => _instance.Action2Event += callback;
+        public static void UnsubscribeFromAction2(Action callback) => _instance.Action2Event -= callback;
+
+        public static void SubscribeToPlayer1Forward(Action callback) => _instance._player1Controls.MoveForwardEvent += callback;
+        public static void UnsubscribeFromPlayer1Forward(Action callback) => _instance._player1Controls.MoveForwardEvent -= callback;
+    }
+
+    [Serializable]
+    public class PlayerInput
+    {
+        public KeyCode _moveForward;
+        public KeyCode _moveBackwards;
+
+        public event Action MoveForwardEvent;
+        public event Action MoveBackwardsEvent;
+
+        public void InvokeForward() => MoveForwardEvent?.Invoke();
+        public void InvokeBackwards() => MoveBackwardsEvent?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using CodeOverload;
+
+public class Tester : MonoBehaviour
+{
+    private void Start()
+    {
+        InputManager.SubscribeToAction1(Action1Callback);
+        InputManager.SubscribeToAction2(Action2Callback);
+
+        InputManager.SubscribeToPlayer1Forward(Player1Forward);
+    }
+
+    private void Action1Callback()
+    {
+        Debug.Log("Executing Action 1");
+    }
+
+    private void Action2Callback()
+    {
+        Debug.Log("Executing Action 2");
+
+        InputManager.UnsubscribeFromAction1(Action1Callback);
+    }
+
+    private void Player1Forward()
+    {
+        Debug.Log("Player 1 Forward");
+    }
+}
